fff1bb6d5570a1237e40e22c3d418b33400a856f
John Carter <johncarter352@gmail.com>
2020-04-05 20:43:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,6 +37,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # add the second argument to the memorial day array
   holiday_hash[:memorial_day].each do |holiday, new|
     new << "Grill"
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
